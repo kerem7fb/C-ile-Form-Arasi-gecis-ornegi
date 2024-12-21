318d8068d3aec4af02b942be31c6e127942dc391
kerem7fb <117513389+kerem7fb@users.noreply.github.com>
2024-12-21 11:41:36 +0300

    Add files via upload

---
+++ formörnek/formörnek/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace formörnek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            
+            if (textBox1.Text == "Kerem" && textBox2.Text == "123")
+            {
+                Form2 yeni = new Form2();
+                yeni.Show();
+                this.Hide();
+                
+            }
+            else 
+            {
+                i++;
+                MessageBox.Show("Kullanıcı Adı Veya Şifre Yanlış");
+                if (i == 3) 
+                {
+                    button1.Enabled = false;
+                }
+
+            }
+           
+
+            
+            
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                textBox2.UseSystemPasswordChar = false;
+            }
+            else
+            {
+                textBox2.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
